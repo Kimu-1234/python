62c9cc2829a233e4ada54eefd9c9ac6442b583aa
root <root@localhost>
2022-09-24 22:02:13 +0300

    auto publish 1664046133587

---
+++ src/main.py
@@ -0,0 +1,14 @@
+k = input("what is your name:")
+print("welcome",k)
+
+def key():
+  age = input("how old are you:")
+  age_1 = int(age)
+  old = 17
+  if old < age_1:
+    print(k,"welcome to pubg")
+  else:
+     print("You cannot play this game if you are not 18 years old")
+    
+  
+key()
